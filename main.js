4681fbc90194288da282447b607e2760c50f9117
josecastroo <castrooj02@gmail.com>
2024-07-24 23:55:17 -0600

    Finished project
    - New HTML, CSS and JavaScript files
    - Redesign of all code logic

--- main.js
+++ main.js
@@ -1,40 +1,72 @@
-function add (a, b) {
-    return a + b;
-}
-function subtract (a, b) {
-    return a - b;
-}
-function multiply (a, b) {
-    return a * b;
-}
-function divide (a, b) {
-    return a / b;
-}
+let operator = ""
+let previousValue = ""
+let currentValue = ""
 
-// console.log(add(34, 6))
-// console.log(subtract(100, 50))
-// console.log(multiply(8, 5))
-// console.log(divide(20, 2))
+document.addEventListener("DOMContentLoaded", function () {
+    let clearBtn = document.querySelector(".clear-button")
+    let equalBtn = document.querySelector(".equal-button")
+    let decimalBtn = document.querySelector(".decimal-button")
 
-let stNum;
-let ndNum;
-let operator;
+    let numbersBtn = document.querySelectorAll(".number-button")
+    let operatorsBtn = document.querySelectorAll(".operator-button")
 
-function operate (operator, num1, num2) {
-    switch (operator) {
-        case "add":
-            return add(num1, num2);
-            break;
-        case "subtract":
-            return subtract(num1, num2);
-            break;
-        case "multiply":
-            return multiply(num1, num2);
-            break;
-        case "divide":
-            return divide(num1, num2);
-            break;
-        default:
-            return "Invalid operator"
+    let previousScreen = document.querySelector(".previous")
+    let currentScreen = document.querySelector(".current")
+
+    numbersBtn.forEach((number) => number.addEventListener("click", function (e) {
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue
+    }))
+
+    operatorsBtn.forEach((op) => op.addEventListener("click", function (e) {
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator
+        currentScreen.textContent = currentValue
+    }))
+
+    clearBtn.addEventListener("click", function () {
+        previousValue = ""
+        currentValue = ""
+        operator = ""
+        previousScreen.textContent = currentValue
+        currentScreen.textContent = currentValue
+    })
+
+    equalBtn.addEventListener("click", function () {
+        if (currentValue != "" && previousValue != "") {
+            calculate()
+            previousScreen.textContent = ""
+            currentScreen.textContent = previousValue
+        }
+    })
+})
+
+function handleNumber (num) {
+    if (currentValue.length <= 5) {
+        currentValue += num
     }
+}
+
+function handleOperator (op) {
+    operator = op
+    previousValue = currentValue
+    currentValue = ""
+}
+
+function calculate () {
+    previousValue = Number(previousValue)
+    currentValue = Number(currentValue)
+    
+    if (operator === "+") {
+        previousValue += currentValue
+    } else if (operator === "-") {
+        previousValue -= currentValue
+    } else if (operator === "x" ) {
+        previousValue *= currentValue
+    } else {
+        previousValue /= currentValue
+    }
+
+    previousValue = previousValue.toString()
+    currentValue = previousValue.toString()
 }
